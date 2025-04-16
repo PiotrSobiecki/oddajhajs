be092f8bb83a54d9187775f9b4f08f89ee0da4c0
PiotrSobiecki <piotr.sobiecki@gameswift.io>
2025-04-16 10:04:16 +0200

    fix logowanie google

--- next.config.js
+++ next.config.js
@@ -14,20 +14,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   poweredByHeader: false, // Usuwanie nagłówka X-Powered-By dla bezpieczeństwa
-  // Opcje konfiguracji dla eksportu
-  experimental: {
-    serverActions: true,
-    // Dodajemy krytyczne funkcje eksperymentalne
-    optimizePackageImports: ["next-auth"],
-  },
-  // Obsługa problemów z eksportem stron klienckich
-  transpilePackages: ["next-auth"],
-  // Wyłączamy eksport strony logowania, która powoduje problemy
-  // (będzie renderowana po stronie serwera)
-  exportPathMap: async function (defaultPathMap) {
-    delete defaultPathMap["/login"];
-    return defaultPathMap;
-  },
 };
 
 module.exports = nextConfig;
